edf91242e600182abc8ebd147fe1056e3886ead2
buddh4 <mail@jharrer.de>
2020-10-19 19:56:15 +0200

    main.js cleanup

--- resources/js/src/main.js
+++ resources/js/src/main.js
@@ -13,16 +13,7 @@ humhub.module('katex', function(module, require, $) {
         });
     };
 
-    window.renderMathInElement = renderMathInElement;
-
-    var hello = function() {
-        alert(module.text('hello')+' - '+module.config.username)
-    };
-
     module.export({
-        //uncomment the following line in order to call the init() function also for each pjax call
-        //initOnPjaxLoad: true,
         init: init,
-        hello: hello
     });
 });
