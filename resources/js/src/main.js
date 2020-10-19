13bd533c6a7630c63348993fc6da2293fe50fc52
buddh4 <mail@jharrer.de>
2020-10-19 19:54:33 +0200

    Removed debugger

--- resources/js/src/main.js
+++ resources/js/src/main.js
@@ -1,14 +1,12 @@
 import katex from "katex/dist/katex";
 import renderMathInElement from "katex/dist/contrib/auto-render";
 
-debugger;
 humhub.module('katex', function(module, require, $) {
 
     var init = function() {
         $(document).on('afterRender', '[data-ui-richtext]', function () {
             var that = this;
             setTimeout(function() {
-                debugger;
                 renderMathInElement(that);
             });
 
